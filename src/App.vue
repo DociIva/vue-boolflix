6693c6d2f1cea327f94dfa48a708595f0765b202
DociIva <78482428+DociIva@users.noreply.github.com>
2021-05-10 15:43:49 +0200

    add-search

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--header-->
+    <header>
+      <Header />
+    </header>
+    
+
+    <!--content-->
+    <main>
+      <Products />
+    </main>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// IMPORTAZIONE DEI COMPONENTI
+import Header from '@/components/Header.vue';
+import Products from '@/components/Products.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Products,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '@/styles/general';
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+      <!--SEARCH E BUTTON-->
+      <!--Logo-->
+       <!-- The form -->
+     <form class="example" action="action_page.php">
+        <input type="text" placeholder="Search.." name="search">
+        <button type="submit">CERCA</button>
+     </form>
+  </div>
+</template>
+
+<script>
+export default {
+   name:'Header',
+}
+</script>
+
+<style scoped lang="scss">
+
+
+</style>
