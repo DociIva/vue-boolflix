42e226b51b6ed9553feaff4d8ac0e8a831d62589
DociIva <78482428+DociIva@users.noreply.github.com>
2021-05-12 17:13:30 +0200

    add-api-2-and-conponent-film

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -1,16 +1,24 @@
 <template>
    <div class="film">
-      
+       <ul>
+          <li>{{info.title}}</li>
+          <li>{{info.original_title}}</li>
+          <li>{{info.original_language}}</li>
+          <li>{{info.vote_average}}</li>
+       </ul>
    </div>
 </template>
 
 <script>
 export default {
    name: 'Film',
+   props: {
+      info: Object,
+   }
    
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 
 </style>
